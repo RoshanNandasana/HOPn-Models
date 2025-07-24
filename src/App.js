56ef78f816308ed6989fc2ac6621ef9d87b4e172
RoshanNandasana <roshannandasana15@gmail.com>
2025-07-24 23:08:07 +0530

    Initial project commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/homepage/Navbar";
+import HeroSection from "./components/homepage/HeroSection";
+import PlatformSection from "./components/homepage/PlatformSection";
+import Footer from "./components/homepage/Footer";
 
-function App() {
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <main>
+        <HeroSection />
+        <PlatformSection />
+      </main>
+      <Footer />
     </div>
-  );
+  )
 }
-
-export default App;
